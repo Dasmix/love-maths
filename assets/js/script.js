f40466e19450233fba73ca940a5841cf80d8d639
Damian Kolacki <dasmixovski@gmail.com>
2024-04-01 12:34:45 +0000

    set up structure of functions

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function()) {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data.type")) === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert('You clicked $(gameType)');
+            }
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswe() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function dipslaySubtractQuestion() {
+
+}
+
+function displayMiltiplyQuestion() {
+
+}
