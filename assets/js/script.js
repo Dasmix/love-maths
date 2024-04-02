16cfa73a29bbf4d66bf438c46c1e0ef9cfaae447
Damian Kolacki <dasmixovski@gmail.com>
2024-04-02 20:16:58 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ function runGame(gameType) {
         displayAddtionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMiltiplyQuestion(num1, num2);
-    } else if (gameType === "") {
+    } else if (gameType === "subtract") {
         dipslaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
@@ -70,7 +70,9 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"]
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Uniplemented operator ${operator}`);
         throw `Uniplemented operator ${operator}. Aborting!`;
@@ -98,8 +100,10 @@ function displayAddtionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function dipslaySubtractQuestion() {
-
+function dipslaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMiltiplyQuestion(operand1, operand2) {
