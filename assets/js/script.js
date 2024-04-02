65132ed27952bba082c35fd608712fc8c01ddb1d
Damian Kolacki <dasmixovski@gmail.com>
2024-04-02 17:24:44 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 
@@ -18,9 +20,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * the main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAddtionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -41,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAddtionQuestion() {
-
+function displayAddtionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function dipslaySubtractQuestion() {
